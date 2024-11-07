6c52edb2be0f906fe2b0dddfa5a10caeba70016f
Virag Savaliya <67185877+viragsavaliya@users.noreply.github.com>
2024-11-06 19:24:50 -0500

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BookIcon from "@mui/icons-material/Book";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BookIcon fontSize="medium" />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
